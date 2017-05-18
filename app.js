cacaa89471178294fca76d8c5ca98bce0f32b7a5
Bowen Zhong <bz4e14@soton.ac.uk>
2017-05-18 12:54:11 +0800

    express static

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
+var path = require('path');
 var express = require('express');
 var app = express();
 var port = process.env.PORT || 8888;
 
-app.use('/', function(req, res) {
-  res.send('Initial Code');
-});
+app.use(express.static(path.join(__dirname, '/client')));
 
 app.listen(port, function() {
   console.log('Express Server is Listening on Port ', port);
