054c4a1b9e067150241783168cdd68a9c5e1a679
zbw322092 <bz4e14@soton.ac.uk>
2017-05-24 22:44:57 +0800

    inital mysql database connection

--- app.js
+++ app.js
@@ -3,6 +3,22 @@ var express = require('express');
 var app = express();
 var port = process.env.PORT || 8888;
 var routers = require('./server/routers.js');
+var mysql = require('mysql');
+
+var connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'yourpassword',
+  database: 'yourdatabase'
+});
+connection.connect(function(err) {
+  if (err) {
+    console.log('error connecting: ' + err.stack);
+    return;
+  }
+
+  console.log('connected as id ' + connection.threadId);
+});
 
 routers(app);
 
